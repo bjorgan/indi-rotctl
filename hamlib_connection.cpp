495b08b142e4fcd929ea8cdda251584d36445f41
Asgeir Bjørgan <asgeirbj@samfundet.no>
2019-06-16 20:43:39 +0200

    Enable arbitrary host/port settings from Connection::TCP fields.

--- hamlib_connection.cpp
+++ hamlib_connection.cpp
@@ -1,5 +1,5 @@
 #include "hamlib_connection.h"
-
+#include <cstring>
 
 HamlibRotator::HamlibRotator(INDI::DefaultDevice* device) : Connection::TCP(device)
 {
@@ -7,7 +7,18 @@ HamlibRotator::HamlibRotator(INDI::DefaultDevice* device) : Connection::TCP(devi
 
 bool HamlibRotator::Connect()
 {
+    const char *hostname = AddressT[0].text;
+    const char *port     = AddressT[1].text;
+
+    //create rotator
     rotator = rot_init(ROT_MODEL_NETROTCTL);
+
+    //set hostname and port of rotator
+    char host_port[FILPATHLEN];
+    snprintf(host_port, FILPATHLEN, "%s:%s", hostname, port);
+    rot_set_conf(rotator, rot_token_lookup(rotator, "rot_pathname"), host_port);
+
+    //connect to rotator
     int retval = rot_open(rotator);
     return retval == RIG_OK;
 }
